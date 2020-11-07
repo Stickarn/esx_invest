8d507060f418abad3fc855a4ea36544e468f8eed
Stickarn <deathlezz@live.com>
2020-11-07 12:09:09 +0100

    Create sv.lua

---
+++ esx_invest/locales/sv.lua
@@ -0,0 +1,10 @@
+Locales['sv'] = {
+    ['invalid_amount'] = '~r~Otillåten Summa',
+    ['broke_amount'] = "~r~Du har inte så mycket pengar på banken",
+    ['sold'] = "~g~Dina aktier har blivit sålda",
+    ['buy'] = "~g~Du har köpt en aktie",
+    ['added'] = "Du har köpt mer aktier i företaget",
+    ['unexpected_error'] = "~r~Ett fel har uppstått",
+    ['open_menu'] = "Tryck ~INPUT_PICKUP~ för att få upp aktiemenyn",
+    ['to_much'] = "~r~Din summa är för mycket, tillåtna summan är max {limit}"
+}
