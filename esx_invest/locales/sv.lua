1bfdf1cdeea330a0650f4fc37dbff03dc6f73fc1
Stickarn <deathlezz@live.com>
2020-11-12 21:58:10 +0100

    Swedish Translation

---
+++ esx_invest/locales/sv.lua
@@ -0,0 +1,10 @@
+Locales['sv'] = {
+    ['invalid_amount'] = '~r~ Ogiltigt belopp',
+    ['broke_amount'] = "~r~ Du har inte det beloppet på din bank",
+    ['sold'] = "~g~ Din aktie har sålts",
+    ['buy'] = "~g~ Du har köpt en andel framgångsrikt",
+    ['added'] = "Du har framgångsrikt investerat mer i din andel",
+    ['unexpected_error'] = "~r~ Ett oväntat fel har inträffat",
+    ['open_menu'] = "Tryck ~INPUT_PICKUP~ för att öppna menyn",
+    ['to_much'] = "~r~ Ditt belopp är för mycket, gränsen är {limit}"
+}
